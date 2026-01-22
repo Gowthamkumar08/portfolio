66d9fb997524018c28a0b8ec0cb15a94f251ca01
Sadiq Ahmed <sadiq@wissenglanz.com>
2026-01-22 16:29:07 +0530

    font changed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="min-h-screen font-sans">
-      <div style={{ fontFamily: '"Bricolage Grotesque", sans-serif', opacity: 0, height: 0 }}>Font Test</div>
       <Navbar />
       <main>
         <Hero />
